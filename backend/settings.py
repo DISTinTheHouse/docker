db6868b2c56fca006d50323703afce524cb26c80
DISTinTheHouse <ufkwear@gmail.com>
2025-08-05 13:05:01 -0600

    url settings

--- backend/settings.py
+++ backend/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
-    "https://docker-gtxf.onrender.com",
+    "https://django-react-wfuu.onrender.com",
 ]
 
 ROOT_URLCONF = 'backend.urls'
